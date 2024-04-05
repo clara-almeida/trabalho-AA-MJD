0a7d4b91bb940c728f467c18d1ea5d87258aa913
clara-almeida <144968459+clara-almeida@users.noreply.github.com>
2024-04-05 17:50:09 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+import requests
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__) # Cria uma instância do Flask. 
+
+
+@app.route("/")
+def apresentacao():
+  return render_template('apresentacao.html')
+
+@app.route("/contato")
+def contato():
+  return render_template('contato.html') 
+
+@app.route("/materias")
+def materias():
+  return render_template('materias.html') 
