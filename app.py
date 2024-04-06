5540266bd69c6e1923eca2deddf706cfa0fa92c5
clara-almeida <clara.almeidagom@gmail.com>
2024-04-06 00:11:08 -0300

    teste 2

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 import requests
 from flask import Flask, render_template, request, redirect, url_for
+from raspador import raspador_infomoney
+
 app = Flask(__name__) # Cria uma instância do Flask. 
 
 
@@ -15,3 +17,28 @@ def contato():
 @app.route("/materias")
 def materias():
   return render_template('materias.html') 
+
+@app.route("/raspador-infomoney")
+def infomoney():
+    html = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+         <title> Raspador Infomoney </title>
+    </head> 
+    <body>
+        <h1> Veja todas as matérias do Infomoney que tem a palavra Petróleo no texto ou no titulo</h1>
+        <p>
+        As matérias encontradas foram:
+        <ul>
+      """
+    for conteudo in raspador_infomoney():
+        if "petróleo" in conteudo:
+            html += f'<li> <a href = "{conteudo["link"]}">{conteudo["titulo"]}'
+    html += """
+        </ul>
+        </p>
+    </body>
+    </html>
+    """
+    return html
--- raspador.py
+++ raspador.py
@@ -1,35 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-#captura página "Ultimas Noticias Infomoney"
-result = requests.get("https://www.infomoney.com.br/ultimas-noticias/")
+def raspador_infomoney(): #captura página "Ultimas Noticias Infomoney"
+    result = requests.get("https://www.infomoney.com.br/ultimas-noticias/")
 
-#variável result vai receber o codigo da execução da página chamada pela request
-soup = BeautifulSoup(result.text)
+    #variável result vai receber o codigo da execução da página chamada pela request
+    soup = BeautifulSoup(result.text)
 
-titles = soup.find_all("span", class_="hl-title hl-title-2")
+    titles = soup.find_all("span", class_="hl-title hl-title-2")
+    conteudo = []
+    # Itera sobre as tags encontradas para extrair os títulos e os links
+    for title in titles:
+        # Extrai o texto do título
+        title_text = title.a.text.strip()
+        # Extrai o link
+        link = title.a["href"]
 
-# Itera sobre as tags encontradas para extrair os títulos e os links
-for title in titles:
-    # Extrai o texto do título
-    title_text = title.a.text.strip()
-    # Extrai o link
-    link = title.a["href"]
+        # Faz uma solicitação HTTP para obter o conteúdo da página do link
+        link_response = requests.get(link)
+        link_html = link_response.text
 
-    # Faz uma solicitação HTTP para obter o conteúdo da página do link
-    link_response = requests.get(link)
-    link_html = link_response.text
+        # Cria um objeto BeautifulSoup para a página do link
+        link_soup = BeautifulSoup(link_html, "html.parser")
 
-    # Cria um objeto BeautifulSoup para a página do link
-    link_soup = BeautifulSoup(link_html, "html.parser")
-
-    # Extrai o conteúdo dos parágrafos principais
-    article = link_soup.find("article", class_= "im-article clear-fix")
-    if article:
-        main_paragraphs = soup.find_all("p")
-        main_text = "\n".join(paragraph.get_text().strip() for paragraph in main_paragraphs)
-
-        print(title_text)
-        print(link)
-        print(main_text)
-        print()
+        # Extrai o conteúdo dos parágrafos principais
+        article = link_soup.find("article", class_= "im-article clear-fix")
+        if article:
+            main_paragraphs = soup.find_all("p")
+            main_text = "\n".join(paragraph.get_text().strip() for paragraph in main_paragraphs)
+        conteudo.append({"titulo": title_text, "link": link, "texto": main_text})
+    return conteudo
